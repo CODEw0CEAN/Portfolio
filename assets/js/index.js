ae84b0450922f858e32c734d7b5825a287ae63fc
OCEAN RAI <sagar.rai2023@outlook.com>
2025-02-21 20:37:45 -0500

    Implemented Contact, about and some modifications to all pages.

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,3 +12,57 @@ navLinks.forEach(link => {
     }
 });
 
+
+// Get data from form
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
+
+// validates the form info
+function validateForm() {
+
+    clearMessage();
+
+    let errorFlag = false;
+
+    if (nameInput.value.length < 1) {
+        errorNodes[0].innerText = "Name cannot be blank";
+        nameInput.classList.add("error-border");
+        errorFlag = true;
+    }
+    if (!emailIsValid(email.value)) {
+        errorNodes[1].innerText = "Invalid email address";
+        email.classList.add("error-border");
+        errorFlag = true;
+    }
+    if (nameInput.value.length < 1) {
+        errorNodes[2].innerText = "Please enter message!";
+        message.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if(!errorFlag) {
+        success.innerText = "You message has been sent. Thank You!";
+    }
+}
+
+// clear error
+function clearMessage() {
+    for (let i = 0; i < errorNodes.length; i++) {
+        errorNodes[i].innerText = "";
+    }
+
+    success.innerText = "";
+
+    nameInput.classList.remove("error-border");
+    email.classList.remove("error-border");
+    message.classList.remove("error-border");
+}
+
+// check if email is valid
+function emailIsValid(email) {
+    let pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattern.test(email);
+}
